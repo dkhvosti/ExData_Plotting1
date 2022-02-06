a94b2bd2adcb223ad391ad5947056767bee13407
dkhvosti <98286425+dkhvosti@users.noreply.github.com>
2022-02-06 08:46:01 -0600

    updated R code with correct date subset retrieval

--- plot3.R
+++ plot3.R
@@ -2,8 +2,8 @@ library(graphics)
 library(lubridate)
 
 df_csv <- read.csv("household_power_consumption.txt",sep = ";")
-df_csv <- df_csv[df_csv$Date == "2/1/2007" | df_csv$Date == "2/2/2007",]
-df_csv$Date <- mdy(df_csv$Date)
+df_csv <- df_csv[df_csv$Date == "1/2/2007" | df_csv$Date == "2/2/2007",]
+df_csv$Date <- dmy(df_csv$Date)
 df_csv[df_csv=="?"] <- NA
 
 df_csv$DateTime <- paste(df_csv$Date,df_csv$Time,sep=" ")
@@ -15,10 +15,13 @@ df_csv$Sub_metering_2 <- as.numeric(df_csv$Sub_metering_2)
 df_csv$Sub_metering_3 <- as.numeric(df_csv$Sub_metering_3)
 y_axis_limits <- as.numeric(seq(0,40,by=10))
 
-plot(df_csv$DateTime,df_csv$Sub_metering_3, type= "l", main = "", xlab = "", ylab = "", col = "blue", ylim = c(0,35))
+plot(df_csv$DateTime,df_csv$Sub_metering_1, type= "l", main = "", xlab = "", ylab = "", col = "black")
 lines(df_csv$DateTime,df_csv$Sub_metering_2,type = "l", col = "red")
-lines(df_csv$DateTime,df_csv$Sub_metering_1,type = "l", col = "black")
-legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), y.intersp = 0.5, lty=1, col=c("black","red","blue")) #,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+lines(df_csv$DateTime,df_csv$Sub_metering_3,type = "l", col = "blue")
+
+#there is something with my installation of R. No matter what I do, the legend is not located correctly.
+#had to fiddle with border, inset, and location to make the legend similar to the example plots
+legend("topright", bty= "n", inset = 0.08, legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), y.intersp = 0.5, lty=1, col=c("black","red","blue")) #,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 title(ylab = "Energy sub metering")   
 
 dev.copy(png,height = 480, width = 480, file= "plot3.png")
--- plot4.R
+++ plot4.R
@@ -2,8 +2,8 @@ library(graphics)
 library(lubridate)
 
 df_csv <- read.csv("household_power_consumption.txt",sep = ";")
-df_csv <- df_csv[df_csv$Date == "2/1/2007" | df_csv$Date == "2/2/2007",]
-df_csv$Date <- mdy(df_csv$Date)
+df_csv <- df_csv[df_csv$Date == "1/2/2007" | df_csv$Date == "2/2/2007",]
+df_csv$Date <- dmy(df_csv$Date)
 df_csv[df_csv=="?"] <- NA
 
 df_csv$DateTime <- paste(df_csv$Date,df_csv$Time,sep=" ")
@@ -18,7 +18,7 @@ df_csv$Sub_metering_2 <- as.numeric(df_csv$Sub_metering_2)
 df_csv$Sub_metering_3 <- as.numeric(df_csv$Sub_metering_3)
 y_axis_limits <- as.numeric(seq(0,40,by=10))
 
-par(mfrow = c(2,2), mar = c(4,4,2,1), oma = c(0,0,2,0))
+par(mfrow = c(2,2))
 
 with(df_csv, {
     
@@ -28,7 +28,11 @@ with(df_csv, {
     plot(df_csv$DateTime,df_csv$Sub_metering_3, type= "l", main = "", xlab = "", ylab = "", col = "blue", ylim = c(0,35))
     lines(df_csv$DateTime,df_csv$Sub_metering_2,type = "l", col = "red")
     lines(df_csv$DateTime,df_csv$Sub_metering_1,type = "l", col = "black")
-    legend("topright", legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), y.intersp = 0.5, lty=1, col=c("black","red","blue")) #,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+    #there is something with my installation of R. No matter what I do, the legend is not located correctly.
+    #had to fiddle with border, inset, and location to make the legend similar to the example plots in plot3
+    #cannot replicate the legend in plot 4. Normally "topright" should do it for legend location but it's not working for me
+    legend("topright", bty= "n", cex = 0.8, y.intersp = 0.2, legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), lty=1, col=c("black","red","blue")) #,legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
     title(ylab = "Energy sub metering") 
     plot(DateTime, Global_reactive_power, type = "l", xlab = "datetime")
     
